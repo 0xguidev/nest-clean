83288e33881365d5a7a6b00d2d24b08ddf088533
0xguidev <guilhermeviniciustrabalho@gmail.com>
2024-10-17 11:32:56 -0300

    Feat: Auth Module with jwt public and private key

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,10 +9,12 @@ import { Env } from "src/env"
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
+      global: true,
       useFactory(config: ConfigService<Env, true>) {
         const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true })
         const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true })
         return {
+          signOptions: { algorithm: "RS256" },
           privateKey: Buffer.from(privateKey, "base64"),
           publicKey: Buffer.from(publicKey, "base64"),
         }
