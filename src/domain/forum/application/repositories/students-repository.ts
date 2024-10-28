0d198b4ab33a1699e4820a5222b866bd16a78b22
0xguidev <guilhermeviniciustrabalho@gmail.com>
2024-10-28 12:51:46 -0300

    Feat: students already exists and wrong creditials errors. student repository. register user and authenticate user use case

--- src/domain/forum/application/repositories/students-repository.ts
+++ src/domain/forum/application/repositories/students-repository.ts
@@ -1,7 +1,6 @@
-import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Student } from '../../enterprise/entities/student'
 
 export abstract class StudentsRepository {
   abstract findByEmail(eamil: string): Promise<Student | null>
-  abstract create(student: Question): Promise<void>
+  abstract create(student: Student): Promise<void>
 }
