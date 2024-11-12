106b7ed11f6b66517465d9434e713c34841104f4
0xguidev <guilhermeviniciustrabalho@gmail.com>
2024-11-12 12:26:54 -0300

    Feat: prisma repositories implemented

--- src/infra/database/prisma/repositories/prisma-answer-attachments-repositories.ts
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repositories.ts
@@ -1,15 +1,27 @@
 import { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
 import { Injectable } from '@nestjs/common'
+import { PrismaServices } from '../prisma.service'
+import { PrismaAnswerAttachmentMapper } from '../mappers/primsa-answer-attachment-mapper'
 
 @Injectable()
 export class PrismaAnwerAttachmentsReporitory
   implements AnswerAttachmentsRepository
 {
-  findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]> {
-    throw new Error('Method not implemented.')
+  constructor(private prisma: PrismaServices) {}
+
+  async findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]> {
+    const questionAttachments = await this.prisma.attachment.findMany({
+      where: { answerId },
+    })
+
+    return questionAttachments.map(PrismaAnswerAttachmentMapper.toDomain)
   }
-  deleteManyByAnswerId(answerId: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async deleteManyByAnswerId(answerId: string): Promise<void> {
+    await this.prisma.attachment.deleteMany({
+      where: {
+        answerId,
+      },
+    })
   }
 }
--- src/infra/database/prisma/repositories/prisma-answers-comments-repositories.ts
+++ src/infra/database/prisma/repositories/prisma-answers-comments-repositories.ts
@@ -1,5 +1,73 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { Injectable } from '@nestjs/common'
+import { PrismaServices } from '../prisma.service'
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { PrismaAnswerCommentMapper } from '../mappers/prisma-answer-mapper'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
 @Injectable()
-export class PrismaAnswersCommentsRepository
-  implements PrismaAnswersCommentsRepository {}
+export class PrismaAnswerCommentsRepository
+  implements AnswerCommentsRepository
+{
+  constructor(private prisma: PrismaServices) {}
+
+  async findById(id: string): Promise<AnswerComment | null> {
+    const answerComment = await this.prisma.comment.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!answerComment) {
+      return null
+    }
+
+    return PrismaAnswerCommentMapper.toDomain(answerComment)
+  }
+
+  async findManyByAnswerId(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<AnswerComment[]> {
+    const answers = await this.prisma.comment.findMany({
+      where: {
+        answerId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return answers.map(PrismaAnswerCommentMapper.toDomain)
+  }
+
+  async create(answerComment: AnswerComment): Promise<void> {
+    const data = PrismaAnswerCommentMapper.toPrisma(answerComment)
+
+    await this.prisma.comment.create({
+      data,
+    })
+  }
+
+  async save(answerComment: AnswerComment): Promise<void> {
+    const data = PrismaAnswerCommentMapper.toPrisma(answerComment)
+
+    await this.prisma.comment.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+  }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    await this.prisma.comment.delete({
+      where: {
+        id: answerComment.id.toString(),
+      },
+    })
+  }
+}
