c7b123a3f7ccd658c70d2c245148e558ce669513
0xguidev <guilhermeviniciustrabalho@gmail.com>
2024-11-22 14:47:00 -0300

    Feat: Comment on question controller and test

---
+++ src/infra/http/controllers/comment-on-question.controller.e2e-spec.ts
@@ -0,0 +1,61 @@
+import { PrismaServices } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { JwtService } from '@nestjs/jwt'
+import request from 'supertest'
+import { AppModule } from '@/infra/app.module'
+import { StudentFactory } from 'test/factories/make-student'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { QuestionFactory } from 'test/factories/make-question'
+
+describe('Comment on question E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaServices
+  let jwt: JwtService
+  let questionFactory: QuestionFactory
+  let studentFactory: StudentFactory
+
+  beforeAll(async () => {
+    const modularRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile()
+
+    app = modularRef.createNestApplication()
+    prisma = modularRef.get(PrismaServices)
+    jwt = modularRef.get(JwtService)
+    questionFactory = modularRef.get(QuestionFactory)
+    studentFactory = modularRef.get(StudentFactory)
+
+    await app.init()
+  })
+
+  test('[POST] /questions/:questionId/comments', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const questionId = question.id.toString()
+
+    const response = await request(app.getHttpServer())
+      .post(`/questions/${questionId}/comments`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New comment',
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    const commentOnDatabase = await prisma.comment.findFirst({
+      where: {
+        content: 'New comment',
+      },
+    })
+
+    expect(commentOnDatabase).toBeTruthy()
+  })
+})
---
+++ src/infra/http/controllers/comment-on-question.controller.ts
@@ -0,0 +1,44 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt-strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
+import { z } from 'zod'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
+
+const commentOnQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+type CommentOnQuestionBodySchema = z.infer<typeof commentOnQuestionBodySchema>
+
+@Controller('/questions/:questionId/comments')
+export class CommentOnQuestionController {
+  constructor(private commentOnQuestion: CommentOnQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(commentOnQuestionBodySchema))
+    body: CommentOnQuestionBodySchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const userId = user.sub
+
+    const result = await this.commentOnQuestion.execute({
+      authorId: userId,
+      questionId,
+      content,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
