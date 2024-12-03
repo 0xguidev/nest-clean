f354d75fffaf0e1687b5066b87a6b26169cd35a8
0xguidev <guilhermeviniciustrabalho@gmail.com>
2024-12-03 17:56:05 -0300

    Feat: upload attachment controller and test

---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+
+@Controller('/attachments')
+export class UploadAttachmentController {
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 50, // 50 MB
+          }),
+          new FileTypeValidator({
+            fileType: '.(png|jpg|jpeg|pdf)',
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file)
+  }
+}
